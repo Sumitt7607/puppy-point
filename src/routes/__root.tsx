2c4203c322a4f3330b0bb1a9809540881a44571c
sumitt7607 <sumitkushwahatg@gmail.com>
2026-07-18 16:00:45 +0530

    feat: add Google Tag Manager (GTM-NWHB9R5X)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -112,6 +112,17 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NWHB9R5X');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         {/* Google Ads tag */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18269924977" />
         <script
@@ -127,6 +138,16 @@ function RootShell({ children }: { children: ReactNode }) {
         <HeadContent />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NWHB9R5X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Scripts />
       </body>
